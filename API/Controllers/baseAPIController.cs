8c387cc8214535b62f53085cd52a7e9f030b67f9
AungKhantMinn <64540252+Aungkhantminn27@users.noreply.github.com>
2022-01-31 11:28:01 +0800

    End of section 2

---
+++ API/Controllers/baseAPIController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // this will go to api/controller
+    public class baseAPIController :ControllerBase 
+    {
+        
+    }
+}
